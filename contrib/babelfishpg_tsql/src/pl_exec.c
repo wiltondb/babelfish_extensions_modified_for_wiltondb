5d3b5a88af52735109e1c8ea4304f72bb08bf5a2
Anju Bharti <66729219+anju15bharti@users.noreply.github.com>
2024-08-02 15:07:26 +0530

    Add quote identifier in search path for multi-db (#2776) (#2825)
    Earlier the database and schema name in format with quotes was not handled in the reset search path as when database and schema was created in format ["abc.df"], it was unable to find the correct search with this database and the creation of some objects like view inside a schema in this database was also not able to reset the search path correctly as it was giving invalid value for parameter.
    
    In this commit, we have added quote identifier in search path to support database name and schema name in quotes for multi-db
    
    Issues Resolved: BABEL-5111
    
    Signed-off-by: ANJU BHARTI <abanju@amazon.com>

--- contrib/babelfishpg_tsql/src/pl_exec.c
+++ contrib/babelfishpg_tsql/src/pl_exec.c
@@ -10284,8 +10284,9 @@ reset_search_path(PLtsql_stmt_execsql *stmt, char **old_search_path, bool *reset
 					*old_search_path = flatten_search_path(path_oids);
 					list_free(path_oids);
 				}
-				new_search_path = psprintf("%s, %s, %s", physical_schema, dbo_schema, *old_search_path);
+				new_search_path = psprintf("%s, %s, %s", quote_identifier(physical_schema), quote_identifier(dbo_schema), *old_search_path);
 
+				pfree(physical_schema);
 				/*
 				 * Add the schema where the object is referenced and dbo
 				 * schema to the new search path
@@ -10293,6 +10294,7 @@ reset_search_path(PLtsql_stmt_execsql *stmt, char **old_search_path, bool *reset
 				(void) set_config_option("search_path", new_search_path,
 										 PGC_USERSET, PGC_S_SESSION,
 										 GUC_ACTION_SAVE, true, 0, false);
+				pfree(new_search_path);
 				return true;
 			}
 			else if (top_es_entry->estate->db_name != NULL && stmt->is_ddl)
@@ -10333,8 +10335,9 @@ reset_search_path(PLtsql_stmt_execsql *stmt, char **old_search_path, bool *reset
 						*old_search_path = flatten_search_path(path_oids);
 						list_free(path_oids);
 					}
-					new_search_path = psprintf("%s, %s, %s", physical_schema, dbo_schema, *old_search_path);
+					new_search_path = psprintf("%s, %s, %s", quote_identifier(physical_schema), quote_identifier(dbo_schema), *old_search_path);
 
+					pfree(physical_schema);
 					/*
 					 * Add the schema where the object is referenced and dbo
 					 * schema to the new search path
@@ -10342,6 +10345,7 @@ reset_search_path(PLtsql_stmt_execsql *stmt, char **old_search_path, bool *reset
 					(void) set_config_option("search_path", new_search_path,
 											 PGC_USERSET, PGC_S_SESSION,
 											 GUC_ACTION_SAVE, true, 0, false);
+					pfree(new_search_path);
 					return true;
 				}
 			}
@@ -10362,8 +10366,9 @@ reset_search_path(PLtsql_stmt_execsql *stmt, char **old_search_path, bool *reset
 			*old_search_path = flatten_search_path(path_oids);
 			list_free(path_oids);
 		}
-		new_search_path = psprintf("%s, %s, %s", physical_schema, dbo_schema, *old_search_path);
+		new_search_path = psprintf("%s, %s, %s", quote_identifier(physical_schema), quote_identifier(dbo_schema), *old_search_path);
 
+		pfree(physical_schema);
 		/*
 		 * Add the schema where the object is referenced and dbo schema to the
 		 * new search path
@@ -10371,8 +10376,11 @@ reset_search_path(PLtsql_stmt_execsql *stmt, char **old_search_path, bool *reset
 		(void) set_config_option("search_path", new_search_path,
 								 PGC_USERSET, PGC_S_SESSION,
 								 GUC_ACTION_SAVE, true, 0, false);
+		pfree(new_search_path);
 		return true;
 	}
+	
+	pfree(cur_dbname);
 	return false;
 }
 
